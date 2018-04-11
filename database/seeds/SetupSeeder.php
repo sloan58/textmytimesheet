6302716c211416601e6a3e2e34a1f25502aebd15
Martin Sloan <martin.sloan@karma-tek.com>
2018-04-11 16:52:53 -0400

    adjust settings and created SetupSeeder

---
+++ database/seeds/SetupSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class SetupSeeder extends Seeder
+{
+    public function run()
+    {
+        \Illuminate\Support\Facades\Artisan::call("migrate:refresh");
+
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // create permissions
+        Permission::create(['name' => 'create permissions']);
+        Permission::create(['name' => 'read permissions']);
+        Permission::create(['name' => 'update permissions']);
+        Permission::create(['name' => 'delete permissions']);
+
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'read users']);
+        Permission::create(['name' => 'update users']);
+        Permission::create(['name' => 'delete users']);
+
+        // create roles and assign created permissions
+
+        $role = Role::create(['name' => 'developer']);
+        $role->givePermissionTo(Permission::all());
+
+        \App\User::create([
+            'name' => 'Marty Sloan',
+            'email' => 'martinsloan58@gmail.com',
+            'phone_number' => env('ADMIN_TELEPHONE'),
+            'password' => bcrypt('p@$$word!')
+        ])->assignRole('developer');
+
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo([
+            'create users',
+            'read users',
+            'update users',
+            'delete users'
+        ]);
+
+    }
+}
