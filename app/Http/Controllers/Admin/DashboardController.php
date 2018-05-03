d5d4780be02d4363955fb925cc79cca43ec299a6
Martin Sloan <martin.sloan@karma-tek.com>
2018-05-03 11:48:12 -0400

    add more charts to dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,28 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\TimeEntryReportJob;
 use App\User;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Request;
+use App\Jobs\TimeEntryReportJob;
+use Colors\RandomColor;
 
 class DashboardController extends Controller
 {
+    /**
+     *  Generate TMTS Dashboard
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
-        $time = [];
-        for($i=7; $i>=0; $i--) {
-            $time[Carbon::now()->subDays($i)->format('m-d-Y')] = \App\Models\TimeEntry::whereBetween('created_at', [
-                Carbon::now()->subDays($i)->startOfDay(),
-                Carbon::now()->subDays($i)->endOfDay()
-            ])->count();
-        }
+        // Top Contributors Chart
+        list($labels, $data) = $this->getTopContributors();
+        $topContributors = app()->chartjs
+            ->name('topContributorsChart')
+            ->type('pie')
+            ->size(['width' => 400, 'height' => 200])
+            ->labels($labels)
+            ->datasets([
+                [
+                    'backgroundColor' => RandomColor::many(count($labels)),
+                    'hoverBackgroundColor' => ['#FF6384', '#36A2EB'],
+                    'data' => $data
+                ]
+            ])
+            ->options([]);
 
-        $labels = array_keys($time);
-        $data = array_values($time);
-
-        $chartjs = app()->chartjs
-            ->name('lineChartTest')
+        // Weekly Hours Chart
+        list($labels, $data) = $this->getHoursThisWeek();
+        $hoursThisWeek = app()->chartjs
+            ->name('hoursThisWeekChart')
             ->type('line')
             ->size(['width' => 400, 'height' => 200])
             ->labels($labels)
@@ -41,9 +53,42 @@ class DashboardController extends Controller
             ])
             ->options([]);
 
-        return view('dashboard', compact('chartjs'));
+        // Bud Light Chart
+        $budLightsThisWeek = app()->chartjs
+            ->name('budLightChart')
+            ->type('line')
+            ->size(['width' => 400, 'height' => 200])
+            ->labels(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',' Saturday'])
+            ->datasets([
+                [
+                    "label" => "Bud Light's Slammed This Week",
+                    'backgroundColor' => "rgba(54, 162, 235, 0.2)",
+                    'borderColor' => "rgba(255,99,132,1)",
+                    "pointBorderColor" => "rgba(255,99,132,1)",
+                    "pointBackgroundColor" => "rgba(255,99,132,1)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => [12, 6, 4, 6, 10, 12, 20],
+                ]
+            ])
+            ->optionsRaw("{
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }");
+
+        return view('dashboard', compact('hoursThisWeek', 'topContributors', 'budLightsThisWeek'));
     }
 
+    /**
+     *  Generate custom Time Entries report
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function report()
     {
         \Log::info("DashboardController@report: Received request for a report");
@@ -58,5 +103,52 @@ class DashboardController extends Controller
         \Alert::success('Report Submitted!  Check your email.')->flash();
 
         return redirect()->back();
+    }
+
+    /**
+     *  Calculate Time Entries submitted this week
+     *
+     * @return array
+     */
+    private function getHoursThisWeek()
+    {
+        $time = [];
+        for ($i = 7; $i >= 0; $i--) {
+            $time[Carbon::now()->subDays($i)->format('m-d-Y')] = \App\Models\TimeEntry::whereBetween('created_at', [
+                Carbon::now()->subDays($i)->startOfDay(),
+                Carbon::now()->subDays($i)->endOfDay()
+            ])->count();
+        }
+
+        $labels = array_keys($time);
+        $data = array_values($time);
+        return array($labels, $data);
+    }
+
+    /**
+     *  Get the top Users by hours submitted
+     *
+     * @return array
+     */
+    private function getTopContributors()
+    {
+        $output = [];
+        $users = User::whereHas('timeEntries', function ($query) {
+            $query->whereBetween('created_at', [
+                Carbon::now()->subDays(6)->startOfDay(),
+                Carbon::now()->endOfDay()
+            ]);
+        })->take(10)->get();
+
+        foreach ($users as $user) {
+            $output[$user->name] = $user->timeEntries()->whereBetween('created_at', [
+                Carbon::now()->subDays(6)->startOfDay(),
+                Carbon::now()->endOfDay()
+            ])->get()->sum('hours');
+        }
+
+        $labels = array_keys($output);
+        $data = array_values($output);
+        return array($labels, $data);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,20 +11,51 @@
         <div class="col-md-12">
             <div class="box box-default">
                 <div class="box-header with-border">
-                    <div class="box-title">Dashboard</div>
-                </div>
-            </div>
-            <button type="button"
-                    class="btn btn-success btn-wd bootstrap-modal-form-open"
-                    data-toggle="modal"
-                    data-target="#reportModal">
+                    <button type="button"
+                            class="btn btn-success btn-wd bootstrap-modal-form-open"
+                            data-toggle="modal"
+                            data-target="#reportModal">
                 <span class="btn-label">
                     <i class="fa fa-plus"></i>
                 </span>
-                Generate Report
-            </button>
-            <div>
-                {!! $chartjs->render() !!}
+                        Generate Report
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Hours This Week</h3>
+                </div>
+                <div class="panel-body">
+                    {!! $hoursThisWeek->render() !!}
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Top Contributors</h3>
+                </div>
+                <div class="panel-body">
+                    {!! $topContributors->render() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Bud Light's Slammed This Week</h3>
+                </div>
+                <div class="panel-body">
+                    {!! $budLightsThisWeek->render() !!}
+                </div>
             </div>
         </div>
     </div>
